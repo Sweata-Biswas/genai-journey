87e28c98b92d289605b4c426463d0b72ab857840
Sweata-Biswas <biswassweata22@gmail.com>
2026-03-13 20:45:18 +0530

    Weather API tool

---
+++ travel-planner-cli/weather_app.py
@@ -0,0 +1,17 @@
+import requests
+city = input("Enter the city name: ")
+url = f"https://wttr.in/{city}?format=j1"
+url_response  = requests.get(url)
+if url_response.ok == True:
+    data = url_response.json()
+else:
+    print("Error fecthing the weather data")    
+temperature = data['current_condition'][0]['temp_C']
+humidity = data['current_condition'][0]['humidity']
+weather_desc = data['current_condition'][0]['weatherDesc'][0]["value"]
+
+
+print("City: "+city)
+print("Temperature: ",temperature,"C")
+print("Humidity: ",humidity)
+print("Weather: ",weather_desc)
