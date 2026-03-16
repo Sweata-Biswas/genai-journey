20ff8c2d1ce5afc78a72915cc9f90cbad412759b
Sweata-Biswas <biswassweata22@gmail.com>
2026-03-16 17:41:17 +0530

    secondary

--- weather-web-app/app.py
+++ weather-web-app/app.py
@@ -8,21 +8,30 @@ app = Flask(__name__)
 #http://127.0.0.1:5000 this is homepage
 def index():
     weather = None
+    error = None
     if request.method == "POST":
         city = request.form["city"]
         url = f"https://wttr.in/{city}?format=j1"
-        response = requests.get(url)
-        data = response.json()
-        temperature = data['current_condition'][0]['temp_C']
-        humidity = data['current_condition'][0]['humidity'] 
-        weather_desc = data['current_condition'][0]['weatherDesc'][0]["value"]
-        weather={
-            'city': city,
-            'temperature':temperature,
-            'humidity': humidity,
-            'description': weather_desc 
-        }
-    return render_template('index.html', weather=weather)
+        try:
+            response = requests.get(url)
+            if response.status_code != 200:
+                raise Exception("API request failed")
+            data = response.json()
+            temperature = data['current_condition'][0]['temp_C']
+            humidity = data['current_condition'][0]['humidity'] 
+            weather_desc = data['current_condition'][0]['weatherDesc'][0]["value"]
+            weather={
+                'city': city,
+                'temperature':temperature,
+                'humidity': humidity,
+                'description': weather_desc 
+            }
+        except Exception as e:
+            error={
+                'city': city,
+                'description': "Enter city name doesn't exists" 
+            }
+    return render_template('index.html', weather=weather,error= error)
 #This starts the server.
 if __name__=='__main__':
     app.run(debug=True)
