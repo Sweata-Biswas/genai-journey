6af94e0b28b546da3a0f5d6f369ba2aaf1247b13
Sweata-Biswas <biswassweata22@gmail.com>
2026-03-17 15:06:09 +0530

    weather icons and UI improvements

--- weather-web-app/app.py
+++ weather-web-app/app.py
@@ -20,11 +20,30 @@ def index():
             temperature = data['current_condition'][0]['temp_C']
             humidity = data['current_condition'][0]['humidity'] 
             weather_desc = data['current_condition'][0]['weatherDesc'][0]["value"]
+            description_lower = weather_desc.lower()
+            if "rain" in description_lower:
+                weather_icon = "🌧️"
+
+            elif "cloud" in description_lower:
+                weather_icon = "☁️"
+
+            elif "sun" in description_lower or "clear" in description_lower:
+                weather_icon = "☀️"
+
+            elif "haze" in description_lower or "mist" in description_lower:
+                weather_icon = "🌫️"
+
+            elif "storm" in description_lower or "thunder" in description_lower:
+                weather_icon = "⛈️"
+
+            else:
+                weather_icon = "🌍"
             weather={
                 'city': city,
                 'temperature':temperature,
                 'humidity': humidity,
-                'description': weather_desc 
+                'description': weather_desc,
+                'icon': weather_icon
             }
         except Exception as e:
             error={
