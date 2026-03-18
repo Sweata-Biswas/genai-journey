3dec6d8c85ba1295f04cfdd65cc413ae3f88fc4a
Sweata-Biswas <biswassweata22@gmail.com>
2026-03-18 23:11:30 +0530

    UI improvement, error handling

--- weather-web-app/app.py
+++ weather-web-app/app.py
@@ -2,10 +2,46 @@ from flask import Flask , request, render_template
 import requests
 #create flask server
 app = Flask(__name__)
+def get_background(description):
+
+    description = description.lower()
+
+    if "rain" in description:
+        return "bg-dark text-white"
+    elif "cloud" in description:
+        return "bg-secondary text-white"
+    elif "clear" in description or "sun" in description:
+        return "bg-warning"
+    elif "haze" in description or "mist" in description:
+        return "bg-light"
+    else:
+        return "bg-info text-white"
+    
+def get_weather_symbol(description):
+    description_lower = description.lower()
+    if "rain" in description_lower:
+        weather_icon = "🌧️"
+
+    elif "cloud" in description_lower:
+        weather_icon = "☁️"
+
+    elif "sun" in description_lower or "clear" in description_lower:
+        weather_icon = "☀️"
+
+    elif "haze" in description_lower or "mist" in description_lower:
+        weather_icon = "🌫️"
+
+    elif "storm" in description_lower or "thunder" in description_lower:
+        weather_icon = "⛈️"
+
+    else:
+        weather_icon = "🌍"
+    return weather_icon
 #Python program → becomes a web server
 @app.route("/", methods=["GET","POST"])
 # When user visits "/" , run this function
 #http://127.0.0.1:5000 this is homepage
+
 def index():
     weather = None
     error = None
@@ -16,41 +52,34 @@ def index():
             response = requests.get(url)
             if response.status_code != 200:
                 raise Exception("API request failed")
-            data = response.json()
-            temperature = data['current_condition'][0]['temp_C']
-            humidity = data['current_condition'][0]['humidity'] 
-            weather_desc = data['current_condition'][0]['weatherDesc'][0]["value"]
-            description_lower = weather_desc.lower()
-            if "rain" in description_lower:
-                weather_icon = "🌧️"
 
-            elif "cloud" in description_lower:
-                weather_icon = "☁️"
-
-            elif "sun" in description_lower or "clear" in description_lower:
-                weather_icon = "☀️"
-
-            elif "haze" in description_lower or "mist" in description_lower:
-                weather_icon = "🌫️"
-
-            elif "storm" in description_lower or "thunder" in description_lower:
-                weather_icon = "⛈️"
+            data = response.json()['data']
+            if "current_condition" in data:
+                current_data =data['current_condition'][0]
+                temperature = current_data.get('temp_C','N/A')
+                humidity = current_data.get('humidity','N/A')
+                weather_desc = current_data.get('weatherDesc',[{}])[0].get("value",'N/A')
+                weather={
+                    'city': city,
+                    'temperature':temperature,
+                    'humidity': humidity,
+                    'description': weather_desc,
+                    'icon': get_weather_symbol(weather_desc),
+                    'background':get_background(weather_desc)
+                    
+                }
 
             else:
-                weather_icon = "🌍"
-            weather={
-                'city': city,
-                'temperature':temperature,
-                'humidity': humidity,
-                'description': weather_desc,
-                'icon': weather_icon
-            }
+                raise Exception("Weather Data not found")
         except Exception as e:
             error={
                 'city': city,
-                'description': "Enter city name doesn't exists" 
+                'description': "Weather data not available" 
             }
     return render_template('index.html', weather=weather,error= error)
+
+
 #This starts the server.
 if __name__=='__main__':
     app.run(debug=True)
+
